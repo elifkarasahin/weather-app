500638ad60474df528f44bc139342f78ce358832
elifkarasahin20@gmail.com <Elif Karaahin>
2026-08-04 19:11:09 +0300

    Weather App temel yapısı geliştirildi, useState ve useEffect eklendi

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 import Header from "./components/Header";
 import WeatherCard from "./components/WeatherCard";
 
+
 function App() {
+
+  const [sehir, setSehir] = useState("İzmir");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    console.log("Component ilk kez açıldı.");
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <WeatherCard />
+
+      <WeatherCard 
+      weather={weather}
+      sehir={sehir}
+      />
     </div>
   );
 }
--- weather-app/src/components/WeatherCard.jsx
+++ weather-app/src/components/WeatherCard.jsx
@@ -1,13 +1,27 @@
-export default function WeatherCard() {
+export default function WeatherCard({ weather, sehir }) {
+
+  if (!weather) {
   return (
     <section className="weather-card">
 
-      <h2>İzmir</h2>
+      <h2>{sehir}</h2>
 
-      <h3>31°C</h3>
+      <h3>--°C</h3>
 
-      <p>☀️ Açık</p>
+      <p>Henüz veri yok.</p>
 
     </section>
   );
+  }
+  return (
+    <section className="weather-card">
+
+      <h2>{sehir}</h2>
+
+      <h3>{weather.current.temperature_2m}°C</h3>
+
+      <p>☀️ Güncel hava durumu</p>
+    </section>
+  );
+
 }
